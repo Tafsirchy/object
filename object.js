9c977da746b72c94b49b2127fb85964f9ee8c731
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-01 09:31:42 +0600

    object intro

---
+++ object.js
@@ -0,0 +1,27 @@
+const age = 21;
+const school = 'CKB';
+const isPassed = true;
+
+const subject = ['HTML', 'CSS', 'JS', 'React', 'Node', 'MongoDB'];
+
+// object 
+
+const bottle = {
+    brand: 'fresh',
+    price: 30,
+    color: 'white',
+    isClean: true
+}
+
+const subject = {
+    name: 'JavaScript',
+    teacher: 'Jhankar sir',
+    duration: '3 months'
+    exmDate: '5th august';
+    chapter: ['first', 'second', 'third']
+    exams: {
+        name: 'final',
+        marks: '100',
+    }
+    
+}
