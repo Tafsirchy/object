834f48b70fa697a689eef5800fd48f896dd46f31
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-01 09:55:14 +0600

    property intro

---
+++ property.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'sodor uddin',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+    favPlaces: ['dhaka', 'chittagong', 'cox bazar'];
+
+}
+// console.log(person);
+console.log(person.profession);
+console.log(person['salary']);
+console.log(person['married'])
