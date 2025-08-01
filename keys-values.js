efb25727b47a259d12c2fd59a693e95ebd29e7f8
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-01 11:29:34 +0600

    keys-values

---
+++ keys-values.js
@@ -0,0 +1,21 @@
+// keys 
+// const computer = {
+//     brand: 'hp',
+//     price: 35000,
+//     processor: 'intel i5',
+//     hdd: '512gb',
+// }
+
+// const keys = Object.keys(computer);
+// console.log(keys);
+
+// values 
+const computer = {
+    brand: 'hp',
+    price: 35000,
+    processor: 'intel i5',
+    hdd: '512gb',
+}
+
+const values = Object.values(computer);
+console.log(values);
